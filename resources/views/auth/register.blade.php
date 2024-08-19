83f85e5c25fc91c0fc699c7f11359908653bef8f
vaneath <vaneathzz@gmail.com>
2024-08-19 18:21:22 +0700

    close #13 Remove rregister route and change order factory

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" class="max-w-lg mx-auto">
         @csrf
 
         <!-- Name -->
